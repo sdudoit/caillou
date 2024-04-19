908ba0992da3f6a696a7d6410509750e28414ffb
Sylvain Dudoit <sdudoit@proton.me>
2024-04-19 16:20:52 +0200

    Add query command

--- caillou/__init__.py
+++ caillou/__init__.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import click
 from dotenv import load_dotenv
 
@@ -9,15 +11,15 @@ def main():
 
 
 @main.command
-@click.argument("TO_LANGUAGE")
+@click.argument("LANGUAGE")
 @click.argument("SENTENCE")
-def translate(to_language, sentence) -> None:
+def tr(language, sentence) -> None:
     """
     Translate into the given language the given sentence
 
     \b
-    TO_LANGUAGE     The language in which the sentence must be translated (e.g. French, English, Dutch, Italian etc.)
-    SENTENCE        The sentence to translate surrounded met quotes (e.g. "This is my sentence to translate")
+    LANGUAGE     The language in which the sentence must be translated (e.g. French, English, Dutch, Italian etc.)
+    SENTENCE     The sentence to translate surrounded with quotes (e.g. "This is my sentence to translate")
     """
     from langchain.chains.llm import LLMChain
     from langchain.prompts import PromptTemplate
@@ -29,92 +31,55 @@ def translate(to_language, sentence) -> None:
     )
     chain = LLMChain(llm=OpenAI(), prompt=prompt)
 
-    response = chain.invoke(input={"language": to_language, "sentence": sentence})
+    response = chain.invoke(input={"language": language, "sentence": sentence})
     print(f"{response['text']}")
 
 
-# @main.command
-# @click.argument("QUESTION")
-# @click.argument(
-#     "PDF_FILE", type=click.Path(exists=True, dir_okay=False, path_type=Path)
-# )
-# def ask(question: str, pdf_file: Path) -> None:
-#     """
-#     Ask a question about the given PDF file
+@main.command
+@click.argument(
+    "PDF_FILE", type=click.Path(exists=True, dir_okay=False, path_type=Path)
+)
+@click.argument("QUERY")
+def query(pdf_file: Path, query: str) -> None:
+    """
+    Submit a query in natural language about the content of the given PDF file
 
-#     \b
-#     QUESTION    A question in natural language
-#     \b
-#     PDF_FILE    PDF file where the AI Assistant must look for information
-#     """
-#
-#     from langchain.chains import RetrievalQA
-#     from langchain.embeddings.base import Embeddings
-#     from langchain.schema import BaseRetriever
-#     from langchain_community.document_loaders import PyPDFLoader
-#     from langchain_community.vectorstores.chroma import Chroma
-#     from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-#     from langchain_text_splitters import RecursiveCharacterTextSplitter
+    \b
+    PDF_FILE    PDF file where the AI Assistant must look for information
+    \b
+    QUERY       A query in natural language about the content of the PDF File surrounded with quotes
+    """
 
-#     load_dotenv()
+    from tempfile import TemporaryDirectory
 
-#     # Load PDF and split in chunks of text
-#     # text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-#     # loader = PyPDFLoader(pdf_file)
-#     # chunks = loader.load_and_split(text_splitter)
-#     embeddings = OpenAIEmbeddings()
+    from langchain.chains import RetrievalQA
+    from langchain_community.document_loaders import PyPDFLoader
+    from langchain_community.vectorstores.chroma import Chroma
+    from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-#     # db = Chroma.from_documents(
-#     #     chunks,
-#     #     embedding=embeddings,
-#     #     persist_directory="tests/embeddings"
-#     # )
+    from .redundant_retriver import RedundantFilterRetriever
 
-#     class RedundantFilterRetriever(BaseRetriever):
-#         embeddings: Embeddings
-#         chroma: Chroma
+    # Load PDF and split in chunks of text
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    loader = PyPDFLoader(pdf_file)
+    chunks = loader.load_and_split(text_splitter)
 
-#         def get_relevant_documents(self, query):
-#             # calculate embeddings for the 'query' string
-#             emb = self.embeddings.embed_query(query)
+    # Define embeddings function to use
+    embeddings = OpenAIEmbeddings()
 
-#             # take embeddings and feed them into that
-#             # max_marginal_relevance_search_by_vector
-#             return self.chroma.max_marginal_relevance_search_by_vector(
-#                 embedding=emb, lambda_mult=0.8
-#             )
+    # Load chunks in DB
+    tmp_dir = TemporaryDirectory("caillou")
+    db = Chroma.from_documents(
+        chunks, embedding=embeddings, persist_directory=tmp_dir.name
+    )
 
-#         async def aget_relevant_documents(self):
-#             return []
+    # Create Chain
+    retriever = RedundantFilterRetriever(embeddings=embeddings, chroma=db)
+    chain = RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(), retriever=retriever, chain_type="stuff"
+    )
 
-#     # Store PDF chunks in Vector DB
-#     db = Chroma(persist_directory="tests/embeddings", embedding_function=embeddings)
-#     # retriever = RedundantFilterRetriever(embeddings=embeddings, chroma=db)
-
-#     chat = ChatOpenAI()
-
-#     chain = RetrievalQA.from_chain_type(
-#         llm=chat, retriever=db.as_retriever(), chain_type="stuff"
-#     )
-
-#     result = chain.invoke(question)
-
-#     print(result["result"])
-
-#     # Chroma.from_documents(embedding=embedding)
-
-#     # for index, chunk in enumerate(chunks, start=1):
-#     #     chunk.metadata = {
-#     #         "page": chunk.metadata["page"],
-#     #         "pdf_name": pdf_file.name,
-#     #     }
-#     #     collection.add(ids=[f"{index}"], metadatas=[chunk.metadata], documents=[chunk.page_content])
-
-#     # # Query
-#     # results = collection.query(
-#     #     query_texts=["Cornerstone"],
-#     #     n_results=3,
-#     # )
-
-#     # for r in results.documents:
-#     #     print(r)
+    # Invoke with query
+    response = chain.invoke(query)
+    print(response["result"])
