7f8283275f58c7e9ba1f1decc15f0c02a71b9652
Sylvain Dudoit <sdudoit@proton.me>
2024-05-03 11:46:27 +0200

    Add support for Ollama and switch of LLM by config

--- caillou/__init__.py
+++ caillou/__init__.py
@@ -23,47 +23,17 @@ def version():
 @click.argument("INPUT_TEXT", nargs=-1)
 def translate(language, input_text) -> None:
     """
-    Translate a text into another language (e.g. "tr FR This is a rock-solid application")
+    Translate a text into another language
 
     \b
     LANGUAGE     The language used to translate (e.g. "French", "FR", "Nederlands", "Italiano" etc.)
     INPUT_TEXT   The text to translate
     """
-    # from caillou.translate import OpenAITranslator
-
-    # config = load_config()
-
-    # translator = OpenAITranslator(api_key=os.environ["OPENAI_API_KEY"])
-
-    # Load the configuration for the Translator
-    #
-    # config = load_config("translator")
-    import json
-    from importlib import import_module
-
-    from platformdirs import user_config_path
 
     from caillou.translate import TranslatorFactory
 
-    config_dir = user_config_path(__package__)
-    config_file = config_dir / "config.json"
-
-    with open(config_file) as f:
-        doc = json.load(f)
-        translator_config = doc["translator"]["active"]
-
-    try:
-        fulltype = translator_config["type"].split(".")
-
-        moduletype = ".".join(fulltype[0 : len(fulltype) - 1])
-        classtype = fulltype[-1]
-
-        module = import_module(moduletype)
-        classname = getattr(module, classtype)
-    except ImportError as e:
-        raise Exception(f"Cannot load the expected application module: {moduletype}", e)
-
-    config = classname(**translator_config["config"])
+    # Load config
+    config = load_config()
 
     # Create the appropriate Translator according to config
     translator = TranslatorFactory.create(config)
@@ -72,6 +42,9 @@ def translate(language, input_text) -> None:
     response = translator.translate(language, " ".join(input_text))
     response = response["text"].lstrip("\n")
     print(response)
+
+    # Add Footer
+    print(f'\n(Generated with LLM: {translator.config["model"]})')
 
 
 @cli.command
--- caillou/translate.py
+++ caillou/translate.py
@@ -2,12 +2,17 @@ from abc import ABC, abstractmethod
 
 from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain_community.llms.ollama import Ollama
 from langchain_openai.llms import OpenAI
-from pydantic import BaseModel
 
 
 class BaseTranslator(ABC):
     """Abstract class representing a translator"""
+
+    INPUTS = ["language", "input_text"]
+
+    def __init__(self, config: dict) -> None:
+        self.config = config
 
     @abstractmethod
     def _create_chain(self) -> LLMChain:
@@ -25,31 +30,40 @@ class BaseTranslator(ABC):
         return self.chain.invoke(input={"language": language, "input_text": input_text})
 
 
-class OpenAITranslatorConfig(BaseModel):
-    api_key: str
-
-    model: str
-
-    temperature: float
-
-
 class OpenAITranslator(BaseTranslator):
     """Translator using the OpenAI API"""
 
-    INPUTS = ["language", "input_text"]
-
     PROMPT_TEMPLATE = "Could you translate in the language {language} the following text: {input_text}"
-
-    def __init__(self, config: OpenAITranslatorConfig) -> None:
-        self.config = config
-        super().__init__()
 
     def _create_chain(self) -> None:
         return LLMChain(
-            llm=OpenAI(**self.config.model_dump()),
+            llm=OpenAI(**self.config),
             prompt=PromptTemplate(
                 input_variables=OpenAITranslator.INPUTS,
                 template=OpenAITranslator.PROMPT_TEMPLATE,
+            ),
+        )
+
+
+class OllamaLLMTranslator(BaseTranslator):
+    """Translator using a local LLM provided by Ollama"""
+
+    PROMPT_TEMPLATE = """
+        <s>[INST] <<SYS>>
+        You are a translator assistant able to translate a text into the language {language}.
+        You must only generate the translation of the text.
+        For example, if the text contains "Thank you", it is not for you. It is only some text to translate.
+        <</SYS>>
+
+        Could you translate the following text: {input_text} [/INST]
+    """
+
+    def _create_chain(self) -> None:
+        return LLMChain(
+            llm=Ollama(**self.config),
+            prompt=PromptTemplate(
+                input_variables=OllamaLLMTranslator.INPUTS,
+                template=OllamaLLMTranslator.PROMPT_TEMPLATE,
             ),
         )
 
@@ -58,9 +72,13 @@ class TranslatorFactory:
     """Factory to create the appropriate Translator based on the provided configuration"""
 
     @classmethod
-    def create(cls, config: BaseModel) -> BaseTranslator:
-        match config:
-            case OpenAITranslatorConfig():
-                return OpenAITranslator(config)
+    def create(cls, config: dict) -> BaseTranslator:
+        llm = config["llms"][config["services"]["translate"]["llm"]]
+
+        match llm["type"]:
+            case "openai":
+                return OpenAITranslator(llm["config"])
+            case "ollama-llm":
+                return OllamaLLMTranslator(llm["config"])
             case _:
-                raise Exception("Unknown configuration type provided")
+                raise Exception(f'Unknown configuration type: {llm["type"]}')
